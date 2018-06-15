dddf182d3bf350e0476043590ee0410f5fe95931
EgerbSher <egorka.k2009@ukr.net>
2018-06-15 10:36:24 +0300

    Initial commit for IASAqueue

---
+++ IASAqueue-Server/IASAqueue-Server/Monitor.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace IASAqueue_Server
+{
+    public partial class Monitor : Form
+    {
+        public Monitor()
+        {
+            InitializeComponent();
+        }
+
+        private void Monitor_KeyUp(object sender, KeyEventArgs e)
+        {
+            
+        }
+
+        private void Monitor_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
